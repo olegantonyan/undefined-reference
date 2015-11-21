b9f136d307181aa985665b693286fa56038ac568
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-21 12:22:31 +0200

    Deploy.sh ...

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@
 
 bundle exec jekyll build
 
+LAST_COMMIT="$(git rev-list --format=%B --max-count=1 HEAD)"
+
 pushd `pwd`
 
 cd /tmp
+rm -rf /tmp/olegantonyan.github.io
 git clone git@github.com:olegantonyan/olegantonyan.github.io.git
 cd olegantonyan.github.io && rm -rf ./*
 
@@ -16,10 +19,7 @@ pushd `pwd`
 
 cd /tmp/olegantonyan.github.io
 git add . --all
-git commit -am 'update'
+git commit -am $LAST_COMMIT
 git push
 
 popd
-
-
-
