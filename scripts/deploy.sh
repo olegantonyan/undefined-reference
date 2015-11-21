750576b25e016e2fe96f48a9032f7048e41dbc85
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-21 13:53:06 +0200

    Travis...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo ${git_user}
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
   cd _site
   git init
+
+  echo ${git_user}
 
   git config user.name "Travis CI"
   git config user.email "oleg.b.antonyan@gmail.com"
@@ -12,8 +15,6 @@ if [[ $TRAVIS_BRANCH == 'master' ]] ; then
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  echo ${git_user}
-  echo ${git_password}
   git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
