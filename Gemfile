0a54b0f92e2667e02a56115968ec9070fe0437ac
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-11-21 14:03:26 +0200

    Travis...

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
-gem "jekyll"
+gem 'jekyll'
+gem 'rake'
 #gem "html-proofer"
 #gem "jekyll-sitemap"
--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bundle exec jekyll build
-#htmlproof ./_site --href-ignore "#,/https:\/\/www\.youtube\.com\/.*/,/http:\/\/www\.youtube\.com\/.*/"
